d45d56812610e1e9d875b096e018f893f3612d45
JasielEfrenJimenezAlvarez <jasiel.jimenez10@gmail.com>
2014-07-08 08:13:53 -0500

    Suma.c

---
+++ suma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+int a,b,c;
+printf("Valor de A:\n");
+scanf("%d",&a);
+printf("Valor de B:\n");
+scanf("%d",&b);
+c=a+b;
+printf("La suma es: %d",c);
+printf("\n");
+}
